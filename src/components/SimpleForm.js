5ef557639b8fa254e3adb4a07509ce60ee677884
Shantovai23 <hasibulhasan23sept@gmail.com>
2021-12-09 20:48:45 +0600

    yup and validation

--- src/components/SimpleForm.js
+++ src/components/SimpleForm.js
@@ -1,33 +1,68 @@
 import React from "react";
 import {useFormik} from 'formik'
+import * as Yup from 'yup'
+
+
+const initialValues={
+  name:'',
+  email:'',
+  city:''
+}
+const onSubmit=values=>{   //es6 function
+  alert(JSON.stringify(values))
+}
+
+const validate=values=>{
+  let errors={}
+
+  if(!values.name){
+    errors.name="Name can't  be empty"
+  }
+  if(!values.email){
+    errors.email="Email can't  be empty"
+  }else if(!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)){
+    errors.email='Invalid email format'
+  }
+  if(!values.city){
+    errors.city="City can't  be empty"
+  }
+
+  return errors
+}
+
+const validationSchema=Yup.object({
+  name:Yup.string().required('Enter your name!'),
+  email:Yup.string()
+  .email("Invalid Email format!")
+  .required('Enter your email!'),
+  city:Yup.string().required("Enter Your City")
+})
 
 const SimpleForm = () => {
 
     const formik=useFormik({
-        initialValues:{
-            name:'',
-            email:'',
-            city:''
-        },
-        onSubmit: (values)=>{
-               alert(JSON.stringify(values))
-        }
+        initialValues,
+        onSubmit,
+        // validate
+        validationSchema
     })
 
-
-
+    console.log(formik.touched);
   return (
     <>
       <div className="formDiv">
         <form onSubmit={formik.handleSubmit}>
           <label htmlFor="name">Name</label>
-          <input type="text" id="name" name="name" onChange={formik.handleChange} value={formik.values.name} />
+          <input type="text" id="name" name="name" onChange={formik.handleChange} value={formik.values.name} onBlur={formik.handleBlur} />
+          {(formik.errors.name && formik.touched.name) && <p>{formik.errors.name}</p>}
 
           <label htmlFor="email">Email</label>
-          <input type="email" id="email" name="email" onChange={formik.handleChange} value={formik.values.email} />
+          <input type="email" id="email" name="email" onChange={formik.handleChange} value={formik.values.email} onBlur={formik.handleBlur} />
+          {(formik.errors.email && formik.touched.email)  && <p>{formik.errors.email}</p>}
 
           <label htmlFor="city">City</label>
-          <input type="text" id="city" name="city" onChange={formik.handleChange} value={formik.values.city}/>
+          <input type="text" id="city" name="city" onChange={formik.handleChange} value={formik.values.city} onBlur={formik.handleBlur}/>
+          {(formik.errors.city && formik.touched.city)  && <p>{formik.errors.city}</p>}
 
           <button type='submit'>SUBMIT</button>
         </form>
